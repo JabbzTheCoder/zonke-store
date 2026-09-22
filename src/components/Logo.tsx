430b162f005af7e80d3549eb63d3d4924bc4a007
JabbzTheCoder <jabbzthecoder@gmail.com>
2026-09-22 04:34:30 +0200

    Logo Updated

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -9,7 +9,7 @@ export default function Logo({ className = "text-3xl", showStore = true }: LogoP
   return (
     <span className={`font-sans font-bold tracking-tight text-white leading-none ${className}`}>
       z<span className="w-[0.65em] h-[0.65em] rounded-full bg-cyan inline-block mx-[0.04em]"></span>nke
-      {showStore && <span className="ml-[0.2em] font-normal">store</span>}
+      {/* {showStore && <span className="ml-[0.2em] font-normal">store</span>} */}
     </span>
   );
 }
